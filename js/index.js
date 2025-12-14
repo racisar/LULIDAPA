8ecd16180c01a4f470e99959dd616c640dcc34e0
racisar <148013983+racisar@users.noreply.github.com>
2025-12-14 19:34:36 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    document.getElementById("navbarLinks").classList.toggle("active");
+}
+
+function goTo(url) {
+    window.open(url, '_blank'); // abre el enlace en nueva pestaña
+}
+
